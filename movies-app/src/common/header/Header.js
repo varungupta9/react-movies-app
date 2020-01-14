9d9c08c86987097d371656dfc7b159797ebded17
varungupta9 <varun4real@gmail.com09quitgit stausexitqgit config --global user.email varun4real@gmail.com>
2020-01-14 22:51:46 +0530

    Added logo

--- movies-app/src/common/header/Header.js
+++ movies-app/src/common/header/Header.js
@@ -1,6 +1,8 @@
 import React ,{Component} from 'react';
 import './Header.css';
 import Button from '@material-ui/core/Button';
+import logo from '../../assets/logo.svg'
+import { isLogicalExpression } from '@babel/types';
 
 class Header extends Component
 {
@@ -8,7 +10,12 @@ class Header extends Component
     {
         return(
             <div>
+            <header className="app-header">
+                <img src={logo} className="app-logo" alt="logo"/>
+            <div className="login-btn">
            <Button variant="contained">LOGIN</Button>
+            </div>
+            </header>
             </div>
         )
     }
